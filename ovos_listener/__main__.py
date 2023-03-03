6c828e7c1ff077d836de461501727fddc0dfbe05
jarbasai <jarbasai@mailfence.com>
2023-03-03 18:55:37 +0000

    feat/bump_ovos_utils

--- ovos_listener/__main__.py
+++ ovos_listener/__main__.py
@@ -13,13 +13,14 @@
 from ovos_config.locale import setup_locale
 from ovos_utils import wait_for_exit_signal
 from ovos_utils.log import init_service_logger
-from ovos_utils.process_utils import PIDLock
+from ovos_utils.process_utils import PIDLock, reset_sigint_handler
 
 from ovos_listener.service import SpeechService, on_error, on_stopping, on_ready
 
 
 def main(ready_hook=on_ready, error_hook=on_error, stopping_hook=on_stopping,
          watchdog=lambda: None):
+    reset_sigint_handler()
     init_service_logger("voice")
     PIDLock("voice")
     setup_locale()
