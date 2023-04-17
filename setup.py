94a55cf646f00dd13c8dae2aab0be5a4bf2099d9
Daniel McKnight <daniel@neongecko.com>
2023-04-17 16:57:17 -0700

    Fix unit test system dependencies

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ def package_files(directory):
     return paths
 
 
-
 def required(requirements_file):
     """ Read requirements file and remove comments and empty lines. """
     with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
